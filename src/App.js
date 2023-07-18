8ad12d0660db31b3c37851709f3b48afe04d0855
Inna <from.inna@gmail.com>
2023-07-17 17:43:31 -1000

    Fetch top market cap coins

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
 
   const fetchAllCoins = async () => {
     const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/list?include_platform=false"
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&locale=en"
     );
     const data = await res.json();
     setAllCoins(data);
