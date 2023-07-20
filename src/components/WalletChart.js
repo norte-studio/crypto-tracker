6afc50627689a971c9ea797adac2823e9ce9842f
Inna <from.inna@gmail.com>
2023-07-19 21:21:16 -1000

    Implemented WalletChart

--- src/components/WalletChart.js
+++ src/components/WalletChart.js
@@ -21,41 +21,55 @@ import moment from 'moment';
     //         ]
     //     }
     // ]
-
+    
+    // walletCoints
+    // [
+    //   {coinId: 'bitcoin', quantity: 10},  
+    //   {coinId: 'etherum', quantity: 5},
+    //]
 export default function WalletChart ({walletCoins, walletCoinPrices}) {
-    // let timeMap = {};
-    // for(let coin of coinPrices) {
-    //     for(let [time, price] of coin.prices) {
-    //         const formatedTime =  moment(time).format('l');
-    //         timeMap[formatedTime] = timeMap[formatedTime] ? 
-    //             {...timeMap[formatedTime], [coin.coinId]: price} :
-    //             {[coin.coinId]: price}
-    //     }
-    // }
-    // console.log(coinPrices);
-    // interm
-    // {
-    //     1682913627610: {bitcon: 30000, eth: 1200},
-    //     1682913627610: {bitcon: 30100, eth: 1100},
-    //     1682917219125: {bitcon: 29000, eth: 1000}
-    // }
-    // let data = Object.keys(timeMap).map(time => ({time, ...timeMap[time]}))
-   
-   
+    let timeMap = {};
+    for(let coin of walletCoinPrices) {
+        for(let [time, price] of coin.prices) {
+            const formatedTime =  moment(time).format('l');
+            timeMap[formatedTime] = timeMap[formatedTime] ? 
+                {...timeMap[formatedTime], [coin.coinId]: price} :
+                {[coin.coinId]: price}
+        }
+    }
+    let data = Object.keys(timeMap).map(time => ({time, ...timeMap[time]}));
+    // almost final
+    //     [
+    //         {time: 1682913627610, bitcoin: 30000, etherum: 1200},
+    //         {time: 1682917219120, bitcoin: 30100, etherum: 1100},
+    //         {time: 1682917219125, bitcoin: 29000, etherum: 1000}
+    //     ]
+   let totalData = data.map(el => {
+        let res = {time: el.time};
+        let total = walletCoins.reduce((total, {coinId, quantity}) => {
+            return total + quantity * el[coinId];
+        }, 0);
+        // let total = 0;
+        // for(const {coinId, quantity} of walletCoins) {
+        //     total += quantity * el[coinId];
+        // }
+        res.total = total;
+        return res;
+   })
     // final
     //     [
     //         {time: 1682913627610, total: 30000},
     //         {time: 1682917219120, total: 30100},
     //         {time: 1682917219125, total: 29000}
     //     ]
-    let data = [];
+    
     
     return (
     <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={500}
           height={300}
-          data={data}
+          data={totalData}
           margin={{
             top: 5,
             right: 30,
